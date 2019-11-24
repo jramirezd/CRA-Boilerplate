df0781fe10788753cb95213c45cd63a089105f74
Jesús Ramírez <jmramirez@intercomempresas.com>
2019-11-24 19:50:04 +0100

    add layout and basic layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import React from 'react';
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Header from './components/Header';
+
+
 function App() {
   return (
-    <div>
-     Boilerplate Jesús ;)
-    </div>
+    <Router>
+      <Header />
+      <main>
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </main>
+      <footer />
+    </Router>
   );
 }
 
